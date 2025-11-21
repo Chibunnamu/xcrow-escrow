9b39f9e6578f18daa61fa35fc5ba975cc33c261f
Chibunnamu <chimjize123@gmail.com>
2025-11-21 18:35:07 +0100

    Trigger Render deployment

--- client/src/pages/CreateTransaction.tsx
+++ client/src/pages/CreateTransaction.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { Card, CardContent, CardHeader, CardTitle } from "../../components/ui/card";
+import { Loader2 } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
