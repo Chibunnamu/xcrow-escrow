13bce4a2451a7688be7f16b2e794b5f65361db09
Chibunnamu <chimjize123@gmail.com>
2025-11-21 10:14:27 +0100

    Revert AuthProvider changes to fix blank page issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AuthProvider } from "./hooks/useAuth";
 import NotFound from "@/pages/not-found";
 import { DashboardLayout } from "@/components/DashboardLayout";
 
